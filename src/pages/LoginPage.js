d7cefa41d46f358da278d4e0571504b1d195082b
전유민 <ychun25@gmail.com>
2025-07-30 03:04:48 +0900

    로그인 페이지 더미 데이터

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -11,62 +11,66 @@ function LoginPage() {
   const [pw, setPw] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    //try {
-    //const response = await axios.post('http://localhost:8080/users/login', {
-    //handle,
-    //password,
-    //});
-
-    console.log('로그인 성공:', response.data);
-    alert(`환영합니다`); //더미 데이터
+  const handleLogin = () => {
+    console.log('로그인 시도 (테스트 모드)');
+    alert(`환영합니다`); // 더미 데이터
     navigate('/main'); // 로그인 성공 후 메인 페이지로 이동
-  }; //catch (error) {
+  };
+  //const handleLogin = async () => {
+  //try {
+  //const response = await axios.post('http://localhost:8080/users/login', {
+  //handle,
+  //password,
+  //});
+
+  // console.log('로그인 성공:', response.data);
+  //alert(`환영합니다`);
+  //navigate('/main'); // 로그인 성공 후 메인 페이지로 이동
+  // }; catch (error) {
   //if (error.response) {
   //alert(`로그인 실패: ${error.response.data.message}`);
   //} else {
   // alert('서버와 연결할 수 없습니다. 잠시 후 다시 시도해주세요.');
   //}
-}
 
-return (
-  <div className="login-page">
-    <StatusBar />
+  return (
+    <div className="login-page">
+      <StatusBar />
 
-    <div className="back-button-container">
-      <button className="back-button" onClick={() => navigate(-1)}>
-        <img src={backIcon} alt="뒤로가기" className="back-icon" />
-      </button>
-    </div>
+      <div className="back-button-container">
+        <button className="back-button" onClick={() => navigate(-1)}>
+          <img src={backIcon} alt="뒤로가기" className="back-icon" />
+        </button>
+      </div>
 
-    <div className="login-wrapper">
-      <div className="brand-box">News To.</div>
+      <div className="login-wrapper">
+        <div className="brand-box">News To.</div>
 
-      <input
-        type="text"
-        placeholder="아이디"
-        className="login-input"
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="비밀번호"
-        className="login-input"
-        value={pw}
-        onChange={(e) => setPw(e.target.value)}
-      />
+        <input
+          type="text"
+          placeholder="아이디"
+          className="login-input"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="login-input"
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+        />
 
-      <button className="login-button" onClick={handleLogin}>
-        로그인
-      </button>
+        <button className="login-button" onClick={handleLogin}>
+          로그인
+        </button>
 
-      <div className="login-links">
-        <span className="link">아이디 찾기</span> |{' '}
-        <span className="link">비밀번호 찾기</span>
+        <div className="login-links">
+          <span className="link">아이디 찾기</span> |{' '}
+          <span className="link">비밀번호 찾기</span>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+}
 export default LoginPage;
